6bbffe79b40465eebdb12c45d7272e1e65d61c67
Oleksandr Yarovyi <aleksanderiarovoy@gmail.com>
2021-10-20 20:49:10 +0300

    shortWeatherInfo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WeatherService} from "./core/services/weather.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'weatherApp';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {RouterModule, Routes} from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component'
+import {HttpClientModule} from "@angular/common/http";
+import { MainComponent } from './shared/components/main/main.component';
+import { WeatherShortInfoComponent } from './shared/components/weather-short-info/weather-short-info.component';
+
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'main', component: MainComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    WeatherShortInfoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
